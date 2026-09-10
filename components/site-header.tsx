28c23e75f445e0dd66e7f507b3d66fe8d19a460d
dsmmarket307 <juanfelipemontanogarcia@gmail.com>
2026-09-09 20:09:00 -0500

    Header mobile: logo centrado tamano compacto, lupa izquierda, menu derecha

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,4 +1,4 @@
-﻿"use client";
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -102,7 +102,7 @@ export default function SiteHeader({ redes }: { redes: Redes }) {
           </button>
 
           <Link href="/" className="flex items-center gap-3 flex-shrink-0 justify-self-center md:justify-self-auto">
-            <Image src="/LOGO-ISRAEL.png" alt="La Biblia Cambia Noticias" width={220} height={70} className="h-32 md:h-16 w-auto object-contain" priority />
+            <Image src="/LOGO-ISRAEL.png" alt="La Biblia Cambia Noticias" width={400} height={130} className="h-10 md:h-16 max-w-none w-auto object-contain" priority />
           </Link>
 
           <nav className="hidden md:flex items-center gap-0.5 font-extrabold text-[13px] flex-shrink-0">
